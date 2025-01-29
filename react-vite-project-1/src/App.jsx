adf00803ab1b2973844458d148558afa6b28cf18
VedeshP <vedeshskp@gmail.com>
2025-01-29 21:22:48 +0530

    first learnings

---
+++ react-vite-project-1/src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+export default function App() {
+  return (
+    <form className="App">
+      <div>
+        <label>Write Something Below</label>
+        <input type="text" className="App-header"/>
+      </div>
+    </form>
+  );
+}
